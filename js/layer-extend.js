77e81469bb37165c11433031f39c7347c36446e6
quyongjian <ahbzqyj521@163.com>
2017-05-02 18:14:49 +0800

    永建周五修改，工作台删除问题，展鹏大哥页面，上门服务-单据详情点击取消

--- js/layer-extend.js
+++ js/layer-extend.js
@@ -8,7 +8,7 @@
 	
 	$.layer={
 			/********************************alert********************************/
-			alert:function(text,options,callback){
+			alert:function(text,options){
 				if(!options){
 					options= {
 							time : 5000,
@@ -17,7 +17,7 @@
 							anim : Math.ceil(Math.random() * 6)
 						}
 				}
-				return layer.alert(text,options,callback);
+				return parent.layer.alert(text,options);
 			},
 			// 成功提示
 			alertS : function(text, title) {
@@ -48,7 +48,7 @@
 			/********************************msg********************************/
 			//信息提示不带按钮
 			msg:function(context,options){
-				return layer.msg(context,options);
+				return parent.layer.msg(context,options);
 			},
 			//信息提示 错误图标
 			msgE:function(context,options){
@@ -65,14 +65,15 @@
 					options={};
 					options.shade=0.1;
 				}
-				return layer.load(icon,options);
+				return parent.layer.load(icon,options);
 			},
 			close:function(index)
 			{
-				layer.close(index);
+				parent.layer.close(index);
 			},
 			/********************************confirm********************************/
 			confirmContext:function(content,title,btn,callBack,options){
+				console.log(callBack);
 				if(!options){
 					options={};
 				}
@@ -80,22 +81,21 @@
 				options.resize=false;
 				options.btn=btn;
 				options.btnAlign='c';
-				layer.confirm(content, options, function(index) {
+				parent.layer.confirm(content, options, function(index) {
 					callBack(index);
 				});
 			},
 			/********************************open********************************/
 			open:function(options){
-				return layer.open(options);
+				return parent.layer.open(options);
 			},
 			//打开一个url
-			openUrl:function(url,title,btn,options){
+			openUrl:function(url,title,options){
 				if(!title) title="信息";
 				if(!options){
 					options={};
 					options.area=["850px","600px"];
 				}
-				if(btn) options.btn=btn;
 				options.type=2;
 				options.content=url;
 				options.title=title;
@@ -103,16 +103,26 @@
 				return $.layer.open(options);
 			},
 			//打开一个div
-			openContent:function(content,title,btn,options){
+			openContent:function(content,title,options){
 				if(!title) title="信息";
 				if(!options){
 					options={};
 					options.area=["550px","400px"];
 				}
-				if(btn) options.btn=btn;
 				options.type=1;
 				options.content=content;
 				options.title=title;
+				options.btnAlign='c';
+				return $.layer.open(options);
+			},
+			//打开一个tab
+			openTab:function(url,options){
+				if(!options){
+					options={};
+					options.area=["850px","600px"];
+				}
+				options.type=2;
+				options.content=url;
 				options.btnAlign='c';
 				return $.layer.open(options);
 			}
